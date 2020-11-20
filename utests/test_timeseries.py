b3806b2f8cf17e59fa2ab9b119b8aabc7a8fdc7c
nada <nada.feki@prevision.com>
2020-11-20 12:10:20 +0100

    Revert "fix: clean testu ts and add compatibility matrix for 10.14"
    This reverts commit 143ab3de194195bf5088657963b963a09157d5bd.

--- utests/test_timeseries.py
+++ utests/test_timeseries.py
@@ -10,6 +10,7 @@ from .utils import get_testing_id
 pio.config.zip_files = True
 
 TESTING_ID = get_testing_id()
+WITH_ALN = False
 
 
 def setup_module():
@@ -71,12 +72,20 @@ def train_model(uc_name, groups=1, time_window=pio.TimeWindow(-90, -30, 1, 15), 
 
 
 windows = [
-    # dws, dwe, fws, fwe
-    (-10, -5, 3, 4),
-    (-90, -30, 1, 15),
-    (-17, -15, 1, 3),
+    # dws, dwe, fws, fwe, aln
+    (-10, -5, 3, 4, False),
+    (-90, -30, 1, 15, False),
+    (-17, -15, 1, 3, False),
 ]
-
+if WITH_ALN:
+    windows = windows + \
+        [
+            # dws, dwe, fws, fwe, aln
+            (-10, -1, 3, 4, True),
+            (-30, -1, 1, 10, True),
+            (-90, -15, 1, 15, True),
+            (-17, -15, 1, 3, True),
+        ]
 
 wrong_windows = [
     (-10, -90, 1, 15),
@@ -99,7 +108,7 @@ def test_ts_groups(groups):
     assert uc.id in [u['usecaseId'] for u in usecases]
 
 
-def time_window_test(dws, dwe, fws, fwe):
+def time_window_test(dws, dwe, fws, fwe, aln=False):
     ts_label = '_'.join(str(s).replace('-', 'm') for s in (dws, dwe, fws, fwe))
     uc_name_asked = 'ts_time{}_{}'.format(ts_label, TESTING_ID)
 
@@ -113,10 +122,10 @@ def time_window_test(dws, dwe, fws, fwe):
     return uc_name_returned
 
 
-@pytest.mark.parametrize('dws, dwe, fws, fwe', windows,
-                         ids=['-'.join(str(s) for s in w) for w in windows])
-def test_time_window(dws, dwe, fws, fwe):
-    uc_name_returned = time_window_test(dws, dwe, fws, fwe)
+@pytest.mark.parametrize('dws, dwe, fws, fwe, aln', windows,
+                         ids=['-'.join(str(s) for s in w[0:-1]) + '-aln={}'.format(w[-1]) for w in windows])
+def test_time_window(dws, dwe, fws, fwe, aln):
+    uc_name_returned = time_window_test(dws, dwe, fws, fwe, aln)
     usecases = [uc['name'] for uc in pio.Supervised.list()]
     assert uc_name_returned in usecases
 
@@ -128,14 +137,17 @@ def test_wrong_time_window(dws, dwe, fws, fwe):
         time_window_test(dws, dwe, fws, fwe)
 
 
-
-ts_params = [(1, False), (3, False)]
-ts_ids = ['no groups-legacy', '3 groups-legacy']
+if WITH_ALN:
+    ts_params = [(1, True), (1, False), (3, True), (3, False)]
+    ts_ids = ['no groups-aln', 'no groups-legacy', '3 groups-aln', '3 groups-legacy']
+else:
+    ts_params = [(1, False), (3, False)]
+    ts_ids = ['no groups-legacy', '3 groups-legacy']
 
 
 @pytest.fixture(scope='class', params=ts_params, ids=ts_ids)
 def setup_ts_class(request):
-    groups = request.param
+    groups, aln = request.param
     group_name = '{}_{}'.format(str(groups[0]), str(groups[1])) if isinstance(groups, tuple) else groups
     uc_name = 'ts_{}grp_{}'.format(group_name, TESTING_ID)
     uc = train_model(uc_name, groups)
