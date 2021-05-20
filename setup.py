95662dcc256e3ea6cc517996c03a5a584ff3735b
Francois Ancel <francois.ancel@prevision.io>
2021-05-20 13:47:53 +0200

    chore: remove reference to unused package Adding pyproject.toml Adding setup.cfg Update setup.py to current standard & remove unused package Rename readme.md to standard naming convention

--- setup.py
+++ setup.py
@@ -1,27 +1,46 @@
+from platform import python_revision
 import sys
 from setuptools import setup, find_packages
 
+
 python_major = sys.version_info[0]
 
-install_requires = ['pandas',
-                    'requests',
-                    'requests-oauthlib',
-                    'apscheduler',
-                    'seaborn',
-                    'scikit-learn',
-                    ]
+install_requires = [
+    "pandas>=1.0.3",
+    "requests>=2.23.0",
+    "scikit-learn>=0.22.2",
+    "numpy>=1.18.4",
+    "requests-oauthlib>=1.3.0",
+    "oauthlib>=3.1.0",
+    "matplotlib>=3.2.1",
+]
 
 if python_major == 2:
     install_requires.append('enum')
 
-setup(name='previsionio',
-      version='11.0.0',
-      description='python wrapper for prevision api',
-      url='',
-      author='prevision.io',
-      author_email='prevision@prevision.io',
-      license='',
-      packages=find_packages(),
-      install_requires=install_requires,
-      zip_safe=False,
-      )
+
+with open("README.md") as readme:
+    setup(name='previsionio',
+          version='11.0.0',
+          description='python wrapper for prevision api',
+          long_description=readme.read(),
+          url='https://github.com/previsionio/prevision-python',
+          author='prevision.io',
+          author_email='prevision@prevision.io',
+          packages=find_packages(exclude=['ci*', 'utest*']),
+          license="MIT License",
+          install_requires=install_requires,
+          zip_safe=False,
+          python_requires=">= 3.6",
+          keywords='ml, ai, prevision, sdk',
+          classifier=[
+              'Intended Audience :: Developers',
+              'Natural Language :: English',
+              'License :: OSI Approved :: Apache Software License',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
+              'Programming Language :: Python :: 3.8',
+              'Programming Language :: Python :: 3.9',
+          ]
+          )
