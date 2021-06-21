736aea6f341edd84793b6df514d9c477b91f1b1c
Florent Rambaud <florent.rambaud@prevision.io>
2021-06-21 12:05:50 +0200

    feat: add retry_codes

--- previsionio/__init__.py
+++ previsionio/__init__.py
@@ -35,6 +35,7 @@ class Config:
     def __init__(self):
         self.auto_update: bool = True
         self.zip_files: bool = True
+        self.retry_codes: list = [500, 502, 503, 504]
         self.request_retries: int = 3
         self.request_retry_time: int = 10
         self.scheduler_refresh_rate: int = 10
