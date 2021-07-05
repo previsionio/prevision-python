9a1a49af4d51651028cc90a983ae86fa47ddb59f
Francois Ancel <francois.ancel@prevision.io>
2021-07-05 15:24:27 +0200

    fix tests

--- utests/test_dataset.py
+++ utests/test_dataset.py
@@ -8,13 +8,11 @@ from . import DATA_PATH
 from .utils import get_testing_id
 
 TESTING_ID = get_testing_id()
-N_DATASETS = 0
 PROJECT_NAME = "sdk_test_dataset_" + str(TESTING_ID)
 PROJECT_ID = ""
 pio.config.zip_files = False
 pio.config.default_timeout = 1000
 
-test_datasets = {}
 paths = {}
 
 
@@ -42,17 +40,17 @@ def test_upload_dataset_from_dataframe():
     for problem_type, p in paths_df.items():
         dataset = project.create_dataset(p.split('/')[-1][:-4] + str(TESTING_ID),
                                          dataframe=pd.read_csv(p))
-        test_datasets[problem_type] = dataset
 
     datasets = [ds for ds in project.list_datasets(all=True) if TESTING_ID in ds.name]
     ds_names = [k + str(TESTING_ID) for k in paths_df]
 
     assert len(datasets) == len(paths_df)
-    global N_DATASETS
-    N_DATASETS += len(datasets)
 
     for ds in datasets:
         assert ds.name in ds_names
+
+    for ds in datasets:
+        ds.delete()
 
 
 def test_upload_dataset_from_dataframe_with_origin():
@@ -61,17 +59,17 @@ def test_upload_dataset_from_dataframe_with_origin():
     for problem_type, p in paths_df.items():
         dataset = project.create_dataset(p.split('/')[-1][:-4] + str(TESTING_ID),
                                          dataframe=pd.read_csv(p), origin="pipeline_intermediate_file")
-        test_datasets[problem_type] = dataset
 
     datasets = [ds for ds in project.list_datasets(all=True) if TESTING_ID in ds.name]
     ds_names = [k + str(TESTING_ID) for k in paths_df]
 
     assert len(datasets) == len(paths_df)
-    global N_DATASETS
-    N_DATASETS += len(datasets)
 
     for ds in datasets:
         assert ds.name in ds_names
+
+    for ds in datasets:
+        ds.delete()
 
 
 def test_upload_dataset_from_filename():
@@ -82,26 +80,37 @@ def test_upload_dataset_from_filename():
                                file_name=p)
 
     datasets = [ds for ds in project.list_datasets(all=True) if TESTING_ID in ds.name]
-    assert len(datasets) == len(paths_files) + N_DATASETS
+    assert len(datasets) == len(paths_files)
+    for ds in datasets:
+        ds.delete()
 
 
 def test_from_id_new():
-    ds = test_datasets['classification']
-    new = pio.Dataset.from_id(ds._id)
-    assert new._id == ds._id
+    project = pio.Project.from_id(PROJECT_ID)
+    dataset = project.create_dataset(paths["classification"].split('/')[-1][:-4] + str(TESTING_ID),
+                                     dataframe=pd.read_csv(paths["classification"]), origin="pipeline_intermediate_file")
+
+    new = pio.Dataset.from_id(dataset._id)
+    assert new._id == dataset._id
+    dataset.delete()
 
 
 def test_download():
-    ds = test_datasets['regression']
-    ds = pio.Dataset.from_id(ds._id)
+    project = pio.Project.from_id(PROJECT_ID)
+    dataset = project.create_dataset(paths["regression"].split('/')[-1][:-4] + str(TESTING_ID),
+                                     dataframe=pd.read_csv(paths["regression"]), origin="pipeline_intermediate_file")
+    ds = pio.Dataset.from_id(dataset._id)
     path = ds.download()
     assert os.path.isfile(path)
     os.remove(path)
+    dataset.delete()
 
 
 def test_embedding():
-    ds = test_datasets['regression']
-    ds = pio.Dataset.from_id(ds._id)
+    project = pio.Project.from_id(PROJECT_ID)
+    ds = project.create_dataset(paths["regression"].split('/')[-1][:-4] + str(TESTING_ID),
+                                dataframe=pd.read_csv(paths["regression"]), origin="pipeline_intermediate_file")
+
     ds.start_embedding()
     t0 = time.time()
     states = [UsecaseState.Pending, UsecaseState.Running]
@@ -112,9 +121,11 @@ def test_embedding():
     assert isinstance(embedding, dict)
     assert 'labels' in embedding
     assert 'tensors' in embedding
+    ds.delete()
 
 
 def test_delete_dataset():
-    ds = test_datasets['regression']
-    ds = pio.Dataset.from_id(ds._id)
+    project = pio.Project.from_id(PROJECT_ID)
+    ds = project.create_dataset(paths["regression"].split('/')[-1][:-4] + str(TESTING_ID),
+                                dataframe=pd.read_csv(paths["regression"]), origin="pipeline_intermediate_file")
     ds.delete()
