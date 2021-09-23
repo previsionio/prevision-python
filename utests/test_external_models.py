6c5b64af1b7be399be922602f99a04e8ba1d683b
lperret <lucas.perret@prevision.io>
2021-09-23 10:10:38 +0200

    test: more external models utests again

--- utests/test_external_models.py
+++ utests/test_external_models.py
@@ -3,7 +3,7 @@ import uuid
 import pandas as pd
 import pytest
 import previsionio as pio
-from previsionio import logger
+from previsionio.model import ExternalRegressionModel, ExternalClassificationModel, ExternalMultiClassificationModel
 
 
 # NOTE: use Unittest classes instead of assert everywhere...
@@ -15,31 +15,20 @@ TESTING_ID = str(uuid.uuid4())
 pio.config.zip_files = False
 pio.config.default_timeout = 1000
 
+type_problems = [
+    'regression',
+    'classification',
+    'multiclassification',
+]
 type_problem_2_projet_usecase_version_creation_method_name = {
-    'regression': "create_external_regression",
-    'classification': "create_external_classification",
-    'multiclassification': "create_external_multiclassification",
+    type_problem: f'create_external_{type_problem}' for type_problem in type_problems
 }
-type_problems = type_problem_2_projet_usecase_version_creation_method_name.keys()
 
 TEST_DATASETS_PATH = {
     'regression': os.path.join(DATA_PATH, 'regression_holdout_dataset.csv'),
     'classification': os.path.join(DATA_PATH, 'classification_holdout_dataset.csv'),
     'multiclassification': os.path.join(DATA_PATH, 'multiclassification_holdout_dataset.csv'),
 }
-
-# just for fast debug with only one type_problem in type_problems
-TEST_DATASETS_PATH = {type_problem: TEST_DATASETS_PATH[type_problem] for type_problem in type_problems}
-
-
-TEST_PIO_DATASETS = {}
-def make_pio_datasets():
-    for problem_type, p in TEST_DATASETS_PATH.items():
-        project = pio.Project.from_id(PROJECT_ID)
-        dataset = project.create_dataset(p.split('/')[-1].replace('.csv', str(TESTING_ID) + '.csv'),
-                                         dataframe=pd.read_csv(p))
-        TEST_PIO_DATASETS[problem_type] = dataset
-
 
 TEST_EXTERNAL_MODELS = {
     'regression': (
@@ -58,6 +47,17 @@ TEST_EXTERNAL_MODELS = {
         os.path.join(DATA_PATH, 'multiclassification_model.yaml'),
     ),
 }
+
+
+TEST_PIO_DATASETS = {}
+
+
+def make_pio_datasets():
+    for problem_type, p in TEST_DATASETS_PATH.items():
+        project = pio.Project.from_id(PROJECT_ID)
+        dataset = project.create_dataset(p.split('/')[-1].replace('.csv', str(TESTING_ID) + '.csv'),
+                                         dataframe=pd.read_csv(p))
+        TEST_PIO_DATASETS[problem_type] = dataset
 
 
 def setup_module(module):
@@ -141,9 +141,7 @@ def test_usecase_version():
 
 def test_usecase_latest_versions():
     usecase_name = f'test_sdk_external_models_test_usecase_latest_versions_{TESTING_ID}'
-    # type_problem = 'classification'
-    # NOTE: set classification at the end to have a different type_problem than in test_usecase_version()
-    type_problem = 'regression'
+    type_problem = 'classification'
     usecase_version = create_external_usecase_version_from_type_problem(type_problem, usecase_name=usecase_name)
 
     usecase_id = usecase_version.usecase_id
@@ -168,9 +166,7 @@ def test_usecase_latest_versions():
 
 def test_stop_running_usecase_version():
     usecase_name = f'test_sdk_external_models_test_stop_running_usecase_version_{TESTING_ID}'
-    # type_problem = 'classification'
-    # NOTE: set multiclassification at the end to have a different type_problem than in test_usecase_version()
-    type_problem = 'regression'
+    type_problem = 'multiclassification'
     usecase_version = create_external_usecase_version_from_type_problem(type_problem, usecase_name=usecase_name)
 
     usecase_id = usecase_version.usecase_id
@@ -185,7 +181,6 @@ def test_stop_running_usecase_version():
 @pytest.fixture(scope='module', params=type_problems)
 def setup_usecase_class(request):
     type_problem = request.param
-    logger.info(f'type_problem: {type_problem}')
     usecase_name = f'test_sdk_external_models_{type_problem}_{TESTING_ID}'
     usecase_version = create_external_usecase_version_from_type_problem(type_problem, usecase_name=usecase_name)
     assert usecase_version.running
@@ -199,7 +194,8 @@ def setup_usecase_class(request):
     pio.Usecase.from_id(usecase_version.usecase_id).delete()
 
 
-# NOTE: atm test nothing else than usecase version launching and stop, which is a part of TestPredict and other tests
+# NOTE: copy paste from test_supervised.py, atm test nothing else than usecase version launching and
+#       stop, which is a part of TestPredict and other tests
 """
 class TestUsecaseVersionGeneric:
 
@@ -220,15 +216,8 @@ class TestPredict:
         type_problem, usecase_version = setup_usecase_class
         dataset_path = TEST_DATASETS_PATH[type_problem]
         data = pd.read_csv(dataset_path)
-        logger.info('start predict')
         preds = usecase_version.predict(data)
         assert len(preds) == len(data)
-        # test_predict_unit
-        # NOTE: predict_unit in external_model ?
-        """
-        pred = usecase_version.predict_single(data.iloc[0].to_dict())
-        assert pred is not None
-        """
 
 
 class TestInfos:
@@ -282,9 +271,11 @@ class TestInfos:
 
         # assert isinstance(model_copy.cross_validation, pd.DataFrame)
         assert isinstance(model_copy.chart(), dict)
-        """
         if type_problem == 'classification':
-            assert isinstance(model_copy, ClassificationModel)
+            assert isinstance(model_copy, ExternalClassificationModel)
             assert model_copy.optimal_threshold == model.optimal_threshold
             assert isinstance(model.get_dynamic_performances(), dict)
-        """
+        elif type_problem == 'multiclassification':
+            assert isinstance(model_copy, ExternalMultiClassificationModel)
+        elif type_problem == 'regression':
+            assert isinstance(model_copy, ExternalRegressionModel)
