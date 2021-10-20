ece737fe558fa20d5a96ad5b7c57f1e74253128c
Florent Rambaud <florent.rambaud@prevision.io>
2021-10-20 16:32:32 +0200

    fix: in predict_single add typing, remove useless 'best' in payload and fix doc

--- previsionio/experiment_version.py
+++ previsionio/experiment_version.py
@@ -646,18 +646,16 @@ class ClassicExperimentVersion(BaseExperimentVersion):
 
         return best.cross_validation
 
-    def predict_single(self,
-                       data,
-                       confidence=False,
-                       explain=False):
-        """ Get a prediction on a single instance using the best model of the experiment.
+    def predict_single(self, data: Dict, confidence: bool = False, explain: bool = False):
+        """ Get a prediction on a single instance using the best model of the experiment. Use
+        :py:func:`predict_from_dataset_name` or :py:func:`predict` methods to predict multiple
+        instances at the same time (it's faster).
 
         Args:
-            use_best_single (bool, optional): Whether to use the best single model
-                instead of the best model overall (default: ``False``)
-            confidence (bool, optional): Whether to predict with confidence values
-                (default: ``False``)
-            explain (bool): Whether to explain prediction (default: ``False``)
+            data (dict): Features names and values (without target feature) - missing feature keys
+                will be replaced by nans
+            confidence (bool, optional): Whether to predict with confidence values (default: ``False``)
+            explain (bool, optional): Whether to explain prediction (default: ``False``)
 
         Returns:
             dict: Dictionary containing the prediction.
@@ -669,9 +667,7 @@ class ClassicExperimentVersion(BaseExperimentVersion):
         """
 
         best = self.best_model
-        return best.predict_single(data,
-                                   confidence=confidence,
-                                   explain=explain)
+        return best.predict_single(data, confidence=confidence, explain=explain)
 
     def predict_from_dataset(self,
                              dataset,
