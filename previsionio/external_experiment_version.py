80b52e9cd88fe9008158d54902e0c515255070a7
Francois Ancel <francois.ancel@prevision.io>
2022-01-26 17:23:34 +0100

    restore import

--- previsionio/external_experiment_version.py
+++ previsionio/external_experiment_version.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import os
 from typing import Dict, List, Tuple, Union
 import requests
+import pandas as pd
 
 from . import metrics
 from .experiment_config import TypeProblem
